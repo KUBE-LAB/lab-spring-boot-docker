cf54014238ad377b837ca88e995780157ea77aac
twinspark01 <twinspark01@outlook.kr>
2022-12-29 23:01:28 +0900

    회원성명입력함수 생성

--- src/main/java/hello/controllers/MainController.java
+++ src/main/java/hello/controllers/MainController.java
@@ -6,24 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
+  /** 루트 호출시 초기화면 로딩  *
+   * 
+   * @return
+  */
   @RequestMapping("/")
   // @ResponseBody
   public String index() {
     return "hello.html";
   }
-
+/**
+ * 메서드명 : 회원이름 조회하기
+ * @return
+ */
   @RequestMapping("/name")
   // @ResponseBody
   public String getName() {
     return "hello.html";
   }
-
-  
+  /** 메서드명: 회원성명 수정함수 **
+   * 
+   * @return
+  */
   @RequestMapping("/update/name")
   // @ResponseBody
   public String updateName() {
     return "hello.html";
   }
+
+ /** 메서드명: 회원성명 입력함수 **
+   * 
+   * @return
+  */
+  @RequestMapping("/input/name")
+  // @ResponseBody
+  public String inputName() {
+    return "hello.html";
+  }
+
 
 }
