55b5cf98d5492eb583541eec018f93c2b26c63c6
twinspark01 <twinspark01@outlook.kr>
2023-01-01 17:13:02 +0900

    멤버 컨트롤러 개

---
+++ src/main/java/hello/controllers/MemberController.java
@@ -0,0 +1,30 @@
+package hello.controllers;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MemberController {
+  /** 루트 호출시 초기화면 로딩  *
+   * 
+   * @return
+  */
+  @RequestMapping("/")
+  // @ResponseBody
+  public String index() {
+    return "hello.html";
+  }
+/**
+ * 메서드명 : 회원이름 조회하기
+ * @return
+ */
+  @RequestMapping("/member")
+  // @ResponseBody
+  public String getMemberName() {
+    return "hello.html";
+  }
+  
+
+
+}
